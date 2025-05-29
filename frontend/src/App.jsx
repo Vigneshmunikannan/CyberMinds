1f7fabc87a7a070b6c3370d01648cfc123c6eeb4
Vigneshmunikannan <107925681+Vigneshmunikannan@users.noreply.github.com>
2025-05-29 14:39:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -317,7 +317,7 @@ function App() {
                       <svg width="13" height="13" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-1 flex-shrink-0">
                         <path d="M18.1728 10.0001L9.99096 15.4546L1.80914 10.0001M18.1728 13.6365L9.99096 19.091L1.80914 13.6365M18.1728 6.36373L9.99096 11.8183L1.80914 6.36373L9.99096 0.90918L18.1728 6.36373Z" stroke="#5A5A5A" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
                       </svg>
-                      <span className="whitespace-nowrap">â‚¹{(job.minSalary / 100000).toFixed(0)} LPA</span>
+                      <span className="whitespace-nowrap">₹ {(job.minSalary / 100000).toFixed(0)} LPA</span>
                     </div>
 
                     <div className="flex items-center">
